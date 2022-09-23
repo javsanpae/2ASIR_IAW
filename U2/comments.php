862a218b3c2c4960980ab611071c218e59b6730e
javsanpae <javiersanchezpaezinformatica@gmail.com>
2022-09-23 10:50:29 +0200

    Feat: New PHP exercises added

---
+++ U2/comments.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit.</h1>
+
+    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+
+
+    <?php
+        # ECHO is used to send content to the nav
+        
+        // We can do comments in these two ways
+        
+        /* We will use this way
+         for multiline comments  */ 
+        echo '<h2>H2 Title</h2>', 'More text', "<p>Welcome to \"Arlandria\"</p>"
+    ?>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, nihil mollitia. Vitae non corrupti, voluptatem a, suscipit maiores tempora sequi ex laboriosam magni modi! Est asperiores possimus architecto! Voluptatem, accusantium voluptate. Deserunt vero rem, est a harum doloribus ducimus soluta!</p>
+</body>
+</html>
---
+++ U2/echos.php
@@ -0,0 +1,4 @@
+<?php
+echo 'Hello World';
+echo "Lorem", "Ipsum", "Dolor sit \"Amet\"" # In order to show "" on a PHP output, we'll put \" on the content we want to show
+?>
